55e782def77f4e3af2e3666406c97feed2dcaf7d
marcos raulino <marcosfsraulino@gmail.com>
2024-01-01 15:31:30 -0300

    doc(api): add DRF Swagger and ReDoc documentation views

---
+++ api/swagger.py
@@ -0,0 +1,33 @@
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='School Manager API',
+        default_version='0.1.0',
+        description='A school manager system to manage courses, students and its enrollments',
+        license=openapi.License(name='GPL-3.0-only'),
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+)
+
+urlpatterns = [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
+]
